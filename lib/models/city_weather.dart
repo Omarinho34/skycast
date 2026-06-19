cec2bd93fb3837c3afb6bb2b3a74d8079fae4a3c
amrio <amri.omar34@outook.fr>
2026-06-19 15:12:19 +0200

    feat(model): CityWeather model

---
+++ lib/models/city_weather.dart
@@ -0,0 +1,33 @@
+import 'weather.dart';
+
+class CityWeather {
+  final String cityName;
+  final Weather weather;
+  final DateTime fetchedAt;
+
+  CityWeather({
+    required this.cityName,
+    required this.weather,
+    required this.fetchedAt,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'cityName': cityName,
+      'weather': {
+        'temperature': weather.temperature,
+        'feelsLike': weather.feelsLike,
+        'tempMin': weather.tempMin,
+        'tempMax': weather.tempMax,
+        'humidity': weather.humidity,
+        'windSpeed': weather.windSpeed,
+        'pressure': weather.pressure,
+        'description': weather.description,
+        'iconCode': weather.iconCode,
+        'sunrise': weather.sunrise.toIso8601String(),
+        'sunset': weather.sunset.toIso8601String(),
+      },
+      'fetchedAt': fetchedAt.toIso8601String(),
+    };
+  }
+}
